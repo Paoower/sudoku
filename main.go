2425eb31ca93d323ff4214470672a489affb2aeb
Emixam26 <maxime.trebert@gmail.com>
2024-05-06 17:45:25 +0200

    v3

--- main.go
+++ main.go
@@ -10,24 +10,33 @@ import (
 func main() {
 	// Récupérer la grille  de Sudoku à partir des arguments donné.
 	board := os.Args[1:]
+
 	// Vérifier que le sudoku est valide.
 	if len(board) != 9 || !isValidBoard(board) {
-		fmt.Println("Erreur la grille n'est pas de bonne taille.")
+		fmt.Println("Erreur la grille n'est pas valide")
 		return
 	}
-	// Créer une copie du sudoku pour vérifier s'il y a plusieurs solutions.
-	boardCopy := make([]string, len(board))
-	copy(boardCopy, board)
+
 	// Essaye de résoudre le Sudoku.
 	if !solveSudoku(board, 0, 0, false) {
 		fmt.Println("Erreur la grille impossible à résoudre.")
 		return
 	}
-	// Vérifie s'il y a plusieurs solutions.
-	if solveSudoku(boardCopy, 0, 0, true) && !areBoardsEqual(board, boardCopy) {
-		fmt.Println("Erreur la grille a plusieurs solution")
-		return
+
+	for _, arg := range board {
+		for _, c := range arg {
+			if c != '.' {
+				// Créer une copie du sudoku pour vérifier s'il y a plusieurs solutions.
+				boardCopy := make([]string, len(board))
+				copy(boardCopy, board)
+				// Vérifie s'il y a plusieurs solutions.
+				if solveSudoku(boardCopy, 0, 0, true) && !areBoardsEqual(board, boardCopy) {
+					fmt.Println("Erreur la grille a plusieurs solution")
+				}
+			}
+		}
 	}
+
 	// Affiche la solution.
 	for _, row := range board {
 		for _, num := range row {
@@ -49,8 +58,11 @@ func areBoardsEqual(board1, board2 []string) bool {
 
 // Vérifie si la grille de Sudoku est valide.
 func isValidBoard(board []string) bool {
+	//ligne
 	for i := 0; i < 9; i++ {
+		//colonne
 		for j := 0; j < 9; j++ {
+			//si n'est pas un . et emplacement du chiffre invalide renvoie false
 			if board[i][j] != '.' && !isValid(board, i, j) {
 				return false
 			}
@@ -61,13 +73,26 @@ func isValidBoard(board []string) bool {
 
 // Vérifie si un chiffre est valide là où il se trouve.
 func isValid(board []string, row, col int) bool {
+	// Vérifier la longueur de chaque ligne
+	for _, row := range board {
+		if len(row) != 9 {
+			return false
+		}
+	}
+	//stock le chiffre
 	num := board[row][col]
+	if num < '1' || num > '9' {
+		return false
+	}
+	//remplace par un .
 	board[row] = board[row][:col] + "." + board[row][col+1:]
 	for i := 0; i < 9; i++ {
+		//si l'emplacement contient deja le chiffre (stocké dans num) renvoie false
 		if board[i][col] == num || board[row][i] == num || board[3*(row/3)+i/3][3*(col/3)+i%3] == num {
 			return false
 		}
 	}
+	//remet le chiffre
 	board[row] = board[row][:col] + string(num) + board[row][col+1:]
 	return true
 }
@@ -81,9 +106,11 @@ func solveSudoku(board []string, row, col int, reverse bool) bool {
 			return true
 		}
 	}
+	//Si case n'est pas vide , appel récursif
 	if board[row][col] != '.' {
 		return solveSudoku(board, row, col+1, reverse)
 	}
+	//En fct du bool reverse définit le sens de résolution
 	start := '1'
 	end := '9'
 	step := 1
@@ -92,15 +119,20 @@ func solveSudoku(board []string, row, col int, reverse bool) bool {
 		end = '1'
 		step = -1
 	}
+	//Test chaque chiffre pour trouver celui qui convient
 	for num := start; ; num = rune(int(num) + step) {
+		// arret si on depasse end
 		if (step > 0 && num > end) || (step < 0 && num < end) {
 			break
 		}
+		// Place le chiffre num actuel dans la grille puis verifie si il convient
 		board[row] = board[row][:col] + string(num) + board[row][col+1:]
 		if isValid(board, row, col) && solveSudoku(board, row, col+1, reverse) {
 			return true
 		}
+		//retire le chiffre si il ne convient pas
 		board[row] = board[row][:col] + "." + board[row][col+1:]
 	}
+	//si ne trouve pas de solution renvoie false
 	return false
 }
